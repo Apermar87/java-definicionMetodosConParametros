94ff5ab24b29efd43ce0df73a1319b74382a56d0
Apermar87 <ajperezmarin@gmail.com>
2024-09-18 13:55:00 +0200

    Ejercicio 2.6 Objetos como parámetros que modifica al ejercicio 2.5 Definición de métodos con parámetros

--- src/App.java
+++ src/App.java
@@ -1,11 +1,20 @@
 public class App {
     public static void main(String[] args) throws Exception {
         CuentaBancaria c1 = new CuentaBancaria("Pepe", "Pérez Domingo", 123456789, CuentaBancaria.tipoCuenta.AHORROS);
+        CuentaBancaria c2 = new CuentaBancaria("Juan", "Gonzalez Huertas", 987654321, CuentaBancaria.tipoCuenta.CORRIENTE);
 
         c1.imprimir();
-        c1.insertarDinero(1000);
+        c1.insertarDinero(2000);
         c1.consultarSaldo();
-        c1.insertarDinero(500);
         c1.retirarDinero(750);
+
+        c2.imprimir();
+        c2.insertarDinero(500);
+        c2.consultarSaldo();
+
+        c1.compararSaldos(c2);
+        c1.transferirDinero(c2, 500);
+
+
     }
 }
--- src/CuentaBancaria.java
+++ src/CuentaBancaria.java
@@ -18,20 +18,20 @@ public class CuentaBancaria {
         System.out.println("Nombre del titular: " + nombre);
         System.out.println("Apellidos del titular: " + apellidos);
         System.out.println("Tipo de cuenta: " + cuenta);
-        System.out.println("Saldo de la cuenta: " + saldo);
+        System.out.println("Saldo de la cuenta: " + saldo + "\n");
     }
 
     void consultarSaldo(){
-        System.out.println("Saldo disponible: " + saldo);
+        System.out.println("Saldo disponible: " + saldo + "\n");
     }
 
     boolean insertarDinero(double ingreso){
         if (ingreso > 0) {
             saldo = saldo + ingreso;
-            System.out.println("Se ha ingresado " + ingreso + " en la cuenta. El nuevo saldo es " + saldo);
+            System.out.println("Se ha ingresado " + ingreso + " en la cuenta. El nuevo saldo es " + saldo + "\n");
             return true;
         } else {
-            System.out.println("El valor del ingreso debe ser mayor que cero.");
+            System.out.println("El valor del ingreso debe ser mayor que cero.\n");
             return false;
         }
     }
@@ -39,12 +39,28 @@ public class CuentaBancaria {
     boolean retirarDinero(double retirar){
        if ((retirar > 0) && (retirar <=saldo)){
         saldo = saldo - retirar;
-        System.out.println("Se ha retirado " + retirar + " en la cuenta. El nuevo saldo es " + saldo);
+        System.out.println("Se ha retirado " + retirar + " en la cuenta. El nuevo saldo es " + saldo + "\n");
         return true;
        } else { 
-        System.out.println("El valor a retirar debe ser menor que el saldo actual.");
+        System.out.println("El valor a retirar debe ser menor que el saldo actual.\n");
         return false;
        }
+    }
+
+    boolean compararSaldos(CuentaBancaria cuenta){
+        if ( saldo>=cuenta.saldo){
+            System.out.println("El saldo de la cuenta 1 es mayor que el saldo de la cuenta 2\n");
+            return true;
+        } else {
+            System.out.println("El saldo de la cuenta 1 es menor que el saldo de la cuenta 2\n");
+            return false;
+        }
+    }
+
+    void transferirDinero(CuentaBancaria cuentaDestino, int valorTransferir){
+        if (retirarDinero(valorTransferir)){
+        cuentaDestino.insertarDinero(valorTransferir);
+        }
     }
     
 }
